bd2c68d297c0e13311824afc76d1d085a93e5a57
saurabhsigma <saurabh962003@gmail.com>
2024-09-03 17:46:22 +0530

    backend done, basic website ready, v1

---
+++ src/components/Message.tsx
@@ -0,0 +1,10 @@
+interface MessageProps {
+    content: string,
+    isUserMessage:boolean
+}
+
+export const Message = ({content, isUserMessage}: MessageProps) =>{
+    return <div className="">
+          
+    </div>
+}
